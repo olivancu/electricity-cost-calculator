ff308d57eaa3456d8618b0e2dc6125f33df02a78
Olivancu <vancutsemolivier@gmail.com>
2018-05-23 14:52:48 -0700

    Dynamic call to the OpenEI API:  - Each site of the meter data is mapped to a set of parameters that defines uniquely the corresponding tariff  - The class OpenEI_tariff makes the API call to get the corresponding data  - The instance of the later is used to analyze the answer and build the CostCalculator structure

--- openei_tariff/openei_tariff_analyzer.py
+++ openei_tariff/openei_tariff_analyzer.py
@@ -1,14 +1,96 @@
+__author__ = 'Olivier Van Cutsem'
 
 from cost_calculator.tariff_structure import *
 from cost_calculator.rate_structure import *
-import pickle  # for test purpose
-import json
+
+import time
 from datetime import datetime
+import requests
 
 # ----------- FUNCTIONS SPECIFIC TO OpenEI REQUESTS -------------- #
 
 
-def tariff_struct_from_openei_data(bill_calculator):
+class OpenEI_tariff(object):
+
+    URL_OPENEI = 'https://api.openei.org/utility_rates'
+    API_KEY = 'BgEcyD9nM0C24J2vL4ezN7ZNAllII0vKA9l7UEBu'
+    FORMAT = 'json'
+    VERSION = 'latest'
+    DIRECTION_SORT = 'asc'
+    DETAIL = 'full'
+    LIMIT = '500'
+    ORDER_BY_SORT = 'startdate'
+
+    def __init__(self, utility_id, sector, tariff_rate_of_interest, distrib_level_of_interest='Secondary', tou=False):
+
+        self.req_param = {}
+
+        # Request param
+        self.req_param['api_key'] = self.API_KEY
+        self.req_param['eia'] = utility_id
+        self.req_param['sector'] = sector
+
+        self.req_param['format'] = self.FORMAT
+        self.req_param['version'] = self.VERSION
+
+        self.req_param['direction'] = self.DIRECTION_SORT
+        self.req_param['detail'] = self.DETAIL
+        self.req_param['limit'] = self.LIMIT
+        self.req_param['orderby'] = self.ORDER_BY_SORT
+
+        # Post-req filter
+        self.tariff_rate_of_interest = tariff_rate_of_interest
+        self.distrib_level_of_interest = distrib_level_of_interest
+        self.tou = tou
+
+        # The raw filtered answer from an API call
+        self.data_openei = None
+
+    def call_api(self):
+
+        r = requests.get(self.URL_OPENEI, params=self.req_param)
+        data_openei = r.json()
+
+        # TODO: this is only valid for the A-10 ! One needs to be more robust and look at the other fields of the answer
+
+        data_filtered = data_openei['items']
+        data_filtered = [v for v in data_filtered if self.tariff_rate_of_interest in v['name']]
+        data_filtered = [v for v in data_filtered if self.distrib_level_of_interest in v['name']]
+        if self.tou:
+            data_filtered = [v for v in data_filtered if 'TOU' in v['name']]
+        # print_json(data_filtered)  # TEST PURPOSE
+
+        # Make sure we work with integer timestamps
+        for rate_data in data_filtered:
+            # Starting time
+            if not (type(rate_data['startdate']) is int):
+                t_s = time.mktime(
+                    datetime.strptime(rate_data['startdate'], '%Y-%m-%dT%H:%M:%S.000Z').timetuple())  # Always specified
+                rate_data['startdate'] = t_s
+
+            # Ending time
+            if 'enddate' in rate_data.keys():
+                if not (type(rate_data['enddate']) is int):
+                    t_e = time.mktime(datetime.strptime(rate_data['enddate'],
+                                                        '%Y-%m-%dT%H:%M:%S.000Z').timetuple())  # maybe not specified - assumed it's until now
+                    rate_data['enddate'] = t_e
+            else:
+                rate_data['enddate'] = time.time()
+
+        # Make sure that the dates are consecutive
+        for i in range(len(data_filtered) - 1):
+            data_cur = data_filtered[i]
+            data_next = data_filtered[i + 1]
+            # Replace END time of the current elem by the START time of the next one if necessary
+            data_cur['enddate'] = min(data_next['startdate'], data_cur['enddate'])
+
+        print "{0} blocks of TOU data, ready to be used !".format(len(data_filtered))
+
+        # Store internally the filtered result
+        self.data_openei = data_filtered
+
+
+def tariff_struct_from_openei_data(openei_tarif_obj, bill_calculator):
     """
     From a set of blocks coming from the OpenEI API, merge the block and extract the useful info
     :return: a formatted structure as explained above
@@ -16,9 +98,7 @@ def tariff_struct_from_openei_data(bill_calculator):
 
     tariff_struct = {}
 
-    openei_structure = pickle.load(open('openei_tariff/tariff_pge_a10_tou.var'))
-
-    for block_rate in openei_structure:
+    for block_rate in openei_tarif_obj.data_openei:
 
         # Tariff starting and ending dates
         tariff_dates = (datetime.fromtimestamp(block_rate['startdate']), datetime.fromtimestamp(block_rate['enddate']))
@@ -31,17 +111,17 @@ def tariff_struct_from_openei_data(bill_calculator):
         if '/day' in block_rate['fixedchargeunits']:
             period_fix_charge = TariffElemPeriod.DAILY
 
-        bill_calculator.add_tariff(FixedTariff(tariff_dates, tariff_fix, period_fix_charge), ChargeType.FIXED)
+        bill_calculator.add_tariff(FixedTariff(tariff_dates, tariff_fix, period_fix_charge), str(TariffType.FIX_CUSTOM_CHARGE.value[0]))
 
         # --- Demand charges
         tariff_demand_obj = get_rate_obj_from_openei(block_rate, ChargeType.DEMAND)
 
-        bill_calculator.add_tariff(TouDemandChargeTariff(tariff_dates, tariff_demand_obj), ChargeType.DEMAND)
+        bill_calculator.add_tariff(TouDemandChargeTariff(tariff_dates, tariff_demand_obj), str(TariffType.DEMAND_CUSTOM_CHARGE_SEASON.value[0]))
 
         # --- Energy charges
         tariff_energy_obj = get_rate_obj_from_openei(block_rate, ChargeType.ENERGY)
 
-        bill_calculator.add_tariff(TouEnergyChargeTariff(tariff_dates, tariff_energy_obj), ChargeType.ENERGY)
+        bill_calculator.add_tariff(TouEnergyChargeTariff(tariff_dates, tariff_energy_obj), str(TariffType.ENERGY_CUSTOM_CHARGE.value[0]))
 
     return tariff_struct
 
--- openei_test.py
+++ openei_test.py
@@ -1,27 +1,32 @@
-from openei_tariff.openei_tariff_analyzer import tariff_struct_from_openei_data
+__author__ = 'Olivier Van Cutsem'
+
 from cost_calculator.cost_calculator import CostCalculator
 import json
 import pandas as pd
-from datetime import datetime
+from openei_tariff.openei_tariff_analyzer import *
 
 # ----------- TEST DEMO -------------- #
 
+# UTILITY ID: {PG&E: 14328, SCE: 17609}
 
+# TODO: read this from a CSV ...
+map_site_to_tariff = {
+    'd3489cfa-93a5-37e7-a274-0f35cf17b782':  # TODO: write the name of the site here
+    OpenEI_tariff(utility_id='14328',
+                  sector='Commercial',
+                  tariff_rate_of_interest='A-10',
+                  distrib_level_of_interest='Secondary',
+                  tou=True)
+}
+# useful functions
 def print_json(json_dict):
     print json.dumps(json_dict, indent=2, sort_keys=True)
-
 
 if __name__ == '__main__':
 
     # Instantiate the bill-calculator object
 
-    bill_calc = CostCalculator()
-
-    # Load the tariff information and fill the object
-
-    tariff_struct = tariff_struct_from_openei_data(bill_calc)
-
-    # Load the energy consumption vector
+    print("--- Loading meter data ...")
 
     df = pd.read_csv('meter.csv', index_col=0)  # import times series energy data for meters
     df.index.name = 'Time'
@@ -32,18 +37,39 @@ if __name__ == '__main__':
 
     meter_uuid = 'd3489cfa-93a5-37e7-a274-0f35cf17b782'
     data_meter = df[meter_uuid]
-    #print data_meter
+
+    # Specify the Utility tariff we're going to analyze
+
+    print("--- Calling API ...")
+    tariff_openei_data = map_site_to_tariff[meter_uuid]  # This points to an object
+
+    tariff_openei_data.call_api()  # This calls the API to internally store the raw data that has to be analyzed
+
+    print("--- Bill calculation ...")
+    bill_calc = CostCalculator()
+
+    # Load the tariff information and fill the object
+
+    tariff_struct = tariff_struct_from_openei_data(tariff_openei_data, bill_calc)  # This analyses the raw data from the openEI request and populate the "CostCalculator" object
+
+    # Load the energy consumption vector
 
     # BILLING PERIOD
-    start_date = datetime(2017, 1, 1, hour=0, minute=0, second=0)
-    end_date = datetime(2017, 3, 31, hour=23, minute=59, second=59)
+    start_date_bill = datetime(2017, 1, 1, hour=0, minute=0, second=0)
+    end_date_bill = datetime(2017, 3, 31, hour=23, minute=59, second=59)
 
-    mask = (data_meter.index >= start_date) & (data_meter.index <= end_date)
-
+    mask = (data_meter.index >= start_date_bill) & (data_meter.index <= end_date_bill)
     data_meter = data_meter.loc[mask]
-
-    # Idea: replace NaN by 0 ?
 
     # 1) Get the bill over the period
 
     print_json(bill_calc.compute_bill(data_meter))
+
+    # 2) Get the electricity price per type of metric, for the 7th of JAN 2017
+
+    start_date_price = datetime(2017, 1, 7, hour=0, minute=0, second=0)
+    end_date_price = datetime(2017, 1, 7, hour=23, minute=59, second=59)
+    range_date = (start_date_price, end_date_price)
+
+    timestep = TariffElemPeriod.HOURLY  # We want a 1h period
+    print bill_calc.get_electricity_price(range_date, timestep)
